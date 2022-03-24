2e735b23811519de0c5557a190a4e2bf8a0dba1d
suke8 <unotean7373@gmail/com>
2022-03-24 14:24:32 +0900

    commit1

---
+++ suke8.py
@@ -0,0 +1,24 @@
+import tweepy
+
+Apikey = "MrXSB2VYQJJYKuLsfbnnG6c2a"
+ApikeySecret = "I5Vbo6jpJDNakeyvcggVqSEfWGxmNxyyBabRyu5c1iTrP5TdFs"
+AccessToken = "1476512981748187137-KrBrg1JD3adzn58V1ClG9ReFShuIqh"
+AccessTokenSecret = "sia6Va8s6AcoDs1ZevZMIg3fI9zMTdMtpo3fVCzM4EOzF"
+
+
+auth =  tweepy.OAuthHandler(Apikey,ApikeySecret)
+auth.set_access_token(AccessToken,AccessTokenSecret)
+
+api = tweepy.API(auth)
+
+objects = api.search_tweets(q="出稼ぎ", result_type="recent", count=5)
+#print(objects)
+
+for object in objects:
+
+    try:
+        retweetId = object.id
+        api.retweet(retweetId)
+
+    except tweepy.TweepError as e:
+        print(e)
